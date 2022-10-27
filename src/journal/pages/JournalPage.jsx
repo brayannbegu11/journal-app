7331397686ec2ec7b0b9bbc75ec9eb649b899bd5
brayannbegu11 <brayannbegu@unisabana.edu.co>
2022-10-27 10:41:25 -0500

    ImageGallery y boton flotante

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,3 +1,5 @@
+import { AddOutlined } from '@mui/icons-material'
+import { IconButton } from '@mui/material'
 import JournalLayout from '../layout/JournalLayout'
 import NoteView from '../views/NoteView'
 import NothingSelectedView from '../views/NothingSelectedView'
@@ -11,9 +13,19 @@ function JournalPage() {
         dicta ipsam nobis saepe sequi, distinctio quis tempore cumque excepturi
         aut ab!
       </Typography> */}
-      {/* <NothingSelectedView /> */}
+      <NothingSelectedView />
 
-      <NoteView />
+      {/* <NoteView /> */}
+      <IconButton size='large' sx={{
+        color: 'white',
+        backgroundColor: 'error.main',
+        ':hover': {backgroundColor: 'error.main', opacity: 0.9},
+        position: 'fixed',
+        right: 20,
+        bottom: 50
+      }}>
+        <AddOutlined sx={{fontSize: 30}}/>
+      </IconButton>
     </JournalLayout>
   )
 }
