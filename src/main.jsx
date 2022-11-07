919fd74329a668c456cd701d9e43fbdaded149df
brayannbegu11 <brayannbegu@unisabana.edu.co>
2022-11-07 15:43:44 -0500

    Configuracion redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
+import {store} from './store';
 import JournalAPP from '../JournalAPP'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <JournalAPP />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <JournalAPP />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 )
